8adae1666ef4cc04ea5b6a448f40fde9cab6c082
Spencer Rafada <spencer.byui@gmail.com>
2022-10-26 21:08:56 -0600

    refactor index.js

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-const express = require("express");
+import express from "express";
 const app = express();
 const port = 5000;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.get("/kushe", (req, res) => {
+  res.send("Kushe o!");
 });
 
 app.listen(port, () => {
